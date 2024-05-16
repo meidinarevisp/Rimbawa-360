1f763dd67bb15e1678c86f505b6a0768c14abefc
meidinarevisp <revimeidina72@gmail.com>
2024-05-16 16:55:49 +0700

    first commit

---
+++ src/scripts/components/header.js
@@ -0,0 +1,26 @@
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const headerEl = document.getElementById("header");
+headerEl.innerHTML = `
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container">
+      <a class="navbar-brand" href="#">Rimbawa 360</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#map">Peta Ekowisata</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#species">Basis Data Spesies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#forum">Forum Diskusi</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+`;
---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import "../styles/style.css";
+import "./components/header";
+import "./components/footer";
+import "./components/map";
+import "./components/species";
+import "./components/forum";
