add7333994e640a53c6670e21fcc171e9b872931
meidinarevisp <revimeidina72@gmail.com>
2024-05-18 15:59:23 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -176,3 +176,19 @@ if (edukasiSection) {
 
   tampilkanEdukasi();
 }
+
+function setActiveNavLink() {
+  const url = window.location.href;
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navLinks.forEach((link) => {
+    if (link.href === url) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+
+// Call the function when the page is loaded
+window.onload = setActiveNavLink;
