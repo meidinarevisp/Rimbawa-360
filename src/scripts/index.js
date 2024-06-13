d863554a263d9c8ab83200702d3b15d6290fdbac
meidinarevisp <revimeidina72@gmail.com>
2024-06-13 11:27:05 +0700

    edit tampilan responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import "regenerator-runtime";
 import "../styles/style.css";
+import "../styles/responsive.css";
 import App from "./views/app";
 import swRegister from "../utils/sw-register";
 import toastr from "toastr";
